5813a64628421b67001790102dbe28a123a3d143
jsyun <yjs3710@hs.ac.kr>
2025-08-15 19:25:30 +0900

    first commit

---
+++ BE/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// 내가 원하는 포트 번호 (예: 5000)
+const PORT = 5000;
+
+// GET 요청 처리
+app.get('/', (req, res) => {
+  res.send('Hello from my custom port! Welcome my server');
+});
+
+// 서버 실행
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
